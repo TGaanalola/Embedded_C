81074c80fa3f6b1c7388cb1d45406f8971ca88e6
TGaanalola <81503646+TGaanalola@users.noreply.github.com>
2021-04-30 17:07:06 +0530

    Update act4.h

--- inc/act4.h
+++ inc/act4.h
@@ -1,1 +1,14 @@
+#ifndef act4_H_INCLUDED
+#define act4_H_INCLUDED
+/** \brief initialize USART
+ *
+ * \param ubrr_value
+ *
+ */
 
+void USARTInit(uint16_t ubrr_value);
+
+void USARTWrite(char temp_value);
+
+char temp_disp();
+#endif // act4_H_INCLUDED

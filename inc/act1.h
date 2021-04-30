b80f2e2f8aa0cfafb89177a724c45e024cc7729a
TGaanalola <81503646+TGaanalola@users.noreply.github.com>
2021-04-30 16:48:06 +0530

    Update act1.h

--- inc/act1.h
+++ inc/act1.h
@@ -1,1 +1,11 @@
+#ifndef act1_H_INCLUDED
+#define act1_H_INCLUDED
 
+#define LED_PORT (PORTB)
+#define IN_PIN1 (PORTB6)
+#define IN_PIN2 (PORTB7)
+#define OUT_PIN (PORTB5)
+void init_port();
+int detectUser();
+
+#endif // act1_H_INCLUDED
